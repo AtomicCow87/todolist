d0b4126346f5be9b396d312eee9103e2d84eec4c
Zach <zkdoran@gmail.com>
2023-06-03 15:51:29 -0400

    create and button

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-
-  var getAndListTasks = function () {
+  var getAndDisplayAllTasks = function () {
     $.ajax({
       type: 'GET',
       url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=207',
       dataType: 'json',
       success: function (response, textStatus) {
+        $('#todo-list').empty(); // Add this line
         response.tasks.forEach(function (task) {
           $('#todo-list').append('<p>' + task.content + '</p>');
         })
@@ -15,6 +15,33 @@ $(document).ready(function(){
       }
     });
   }
-
-  getAndListTasks();
+  
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=207',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        $('#new-task-content').val('');
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });  
+  }
+  
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
+  });
+  
+  getAndDisplayAllTasks();
+  
 });
